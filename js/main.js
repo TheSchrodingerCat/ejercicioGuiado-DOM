79148ddcda3a62fd9fc8d1ab467694e81025c432
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-04-25 13:02:50 -0300

    variaciones al HTML

--- js/main.js
+++ js/main.js
@@ -5,10 +5,15 @@ function translate(){
 	var remember = document.getElementsByTagName("span")[0];
 	var button = document.getElementsByClassName("btn")[0];
 
+	var valorEmail = document.getElementById('inputEmail').value;
+	var valorPassword = document.getElementById("inputPassword").value;
+
 	title.innerHTML = "Por favor inicia sesión";
 	email.placeholder = "Correo Electrónico";
 	password.placeholder = "Contraseña";
 	remember.innerHTML = "Recordar datos";
-	button.innerHTML = "Iniciar Sesión"
+	button.innerHTML = "Iniciar Sesión";
+
+	document.write(valorEmail);
 }
 translate();
