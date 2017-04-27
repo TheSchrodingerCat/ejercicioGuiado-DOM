f2de785a78459e5f602051a0cf2b596832c5b70d
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-04-27 00:36:53 -0600

    ahora imprime en pantalla por 1 segundo

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ function translate(){
 	var remember = document.getElementsByTagName("span")[0];
 	var button = document.getElementsByClassName("btn")[0];
 
-	var valorEmail = document.getElementById('inputEmail').value;
-	var valorPassword = document.getElementById("inputPassword").value;
-	var printEmail = document.getElementById('correo');
-	var printClave = document.getElementById('clave');
+	//var valorEmail = 
+	//var valorPassword = 
+	//var printEmail = document.getElementById('correo');
+	//var printClave = document.getElementById('clave');
 
 	title.innerHTML = "Por favor inicia sesión";
 	email.placeholder = "Correo Electrónico";
@@ -16,7 +16,14 @@ function translate(){
 	remember.innerHTML = "Recordar datos";
 	button.innerHTML = "Iniciar Sesión";
 
-	printEmail.innerHTML = email.value;
-	printClave.innerHTML = password.value;
+	//printEmail.innerHTML = valorEmail;
+	//printClave.innerHTML = valorPassword;
 }
 translate();
+
+function imprimirDatos(){
+	var printEmail = document.getElementById('inputEmail').value;
+	var printClave = document.getElementById("inputPassword").value;
+	document.getElementById('correo').innerHTML = printEmail;
+	document.getElementById('clave').innerHTML = printClave;
+}
